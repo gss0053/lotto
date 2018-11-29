9d0fa4ff38d0bdb102a2a74b62dad3c05e611299
wonuk <wonuk007@gmail.com>
2018-11-29 18:20:10 +0900

    변호별 통계 파트 -(소정민 11.29 PM 6 20)

--- Lotto/Frm_ByNumberChart.Designer.cs
+++ Lotto/Frm_ByNumberChart.Designer.cs
@@ -28,47 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.ByNumberChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.cbo_Start_Number = new System.Windows.Forms.ComboBox();
+            this.cbo_End_Number = new System.Windows.Forms.ComboBox();
+            this.btn_sub = new System.Windows.Forms.Button();
+            this.chk_Bonus = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ByNumberChart)).BeginInit();
             this.SuspendLayout();
             // 
             // ByNumberChart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.ByNumberChart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.ByNumberChart.Legends.Add(legend2);
-            this.ByNumberChart.Location = new System.Drawing.Point(12, 21);
+            chartArea1.Name = "ChartArea1";
+            this.ByNumberChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.ByNumberChart.Legends.Add(legend1);
+            this.ByNumberChart.Location = new System.Drawing.Point(12, 89);
             this.ByNumberChart.Name = "ByNumberChart";
             this.ByNumberChart.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.ByNumberChart.Series.Add(series2);
-            this.ByNumberChart.Size = new System.Drawing.Size(300, 300);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series1.Legend = "Legend1";
+            series1.MarkerSize = 1;
+            series1.Name = "Series1";
+            this.ByNumberChart.Series.Add(series1);
+            this.ByNumberChart.Size = new System.Drawing.Size(763, 571);
             this.ByNumberChart.TabIndex = 0;
             this.ByNumberChart.Text = "chart1";
+            // 
+            // cbo_Start_Number
+            // 
+            this.cbo_Start_Number.FormattingEnabled = true;
+            this.cbo_Start_Number.Location = new System.Drawing.Point(249, 47);
+            this.cbo_Start_Number.Name = "cbo_Start_Number";
+            this.cbo_Start_Number.Size = new System.Drawing.Size(121, 20);
+            this.cbo_Start_Number.TabIndex = 1;
+            // 
+            // cbo_End_Number
+            // 
+            this.cbo_End_Number.FormattingEnabled = true;
+            this.cbo_End_Number.Location = new System.Drawing.Point(411, 47);
+            this.cbo_End_Number.Name = "cbo_End_Number";
+            this.cbo_End_Number.Size = new System.Drawing.Size(121, 20);
+            this.cbo_End_Number.TabIndex = 2;
+            // 
+            // btn_sub
+            // 
+            this.btn_sub.Location = new System.Drawing.Point(585, 45);
+            this.btn_sub.Name = "btn_sub";
+            this.btn_sub.Size = new System.Drawing.Size(75, 23);
+            this.btn_sub.TabIndex = 3;
+            this.btn_sub.Text = "출력";
+            this.btn_sub.UseVisualStyleBackColor = true;
+            this.btn_sub.Click += new System.EventHandler(this.btn_sub_Click);
+            // 
+            // chk_Bonus
+            // 
+            this.chk_Bonus.AutoSize = true;
+            this.chk_Bonus.Location = new System.Drawing.Point(141, 53);
+            this.chk_Bonus.Name = "chk_Bonus";
+            this.chk_Bonus.Size = new System.Drawing.Size(15, 14);
+            this.chk_Bonus.TabIndex = 4;
+            this.chk_Bonus.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "회차선택";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "보너스 미포함";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(376, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "부터";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(538, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "까지";
             // 
             // Frm_ByNumberChart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 672);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chk_Bonus);
+            this.Controls.Add(this.btn_sub);
+            this.Controls.Add(this.cbo_End_Number);
+            this.Controls.Add(this.cbo_Start_Number);
             this.Controls.Add(this.ByNumberChart);
             this.Name = "Frm_ByNumberChart";
             this.Text = "Frm_ByNumberChart";
             this.Load += new System.EventHandler(this.Frm_ByNumberChart_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ByNumberChart)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart ByNumberChart;
+        private System.Windows.Forms.ComboBox cbo_Start_Number;
+        private System.Windows.Forms.ComboBox cbo_End_Number;
+        private System.Windows.Forms.Button btn_sub;
+        private System.Windows.Forms.CheckBox chk_Bonus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Lotto/Frm_ByNumberChart.cs
+++ Lotto/Frm_ByNumberChart.cs
@@ -12,13 +12,21 @@ namespace Lotto
 {
     public partial class Frm_ByNumberChart : Form
     {
-        int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
-        int num11, num12, num13, num14, num15, num16, num17, num18, num19, num20;
-        int num21, num22, num23, num24, num25, num26, num27, num28, num29, num30;
-        int num31, num32, num33, num34, num35, num36, num37, num38, num39, num40;
-        int num41, num42, num43, num44, num45;
+        //int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+        //int num11, num12, num13, num14, num15, num16, num17, num18, num19, num20;
+        //int num21, num22, num23, num24, num25, num26, num27, num28, num29, num30;
+        //int num31, num32, num33, num34, num35, num36, num37, num38, num39, num40;
+        //int num41, num42, num43, num44, num45;
 
+        //int[] count = {0,0,0,0,0, 0,0,0,0,0,
+        //               0,0,0,0,0, 0,0,0,0,0,
+        //               0,0,0,0,0, 0,0,0,0,0,
+        //               0,0,0,0,0, 0,0,0,0,0,
+        //               0,0,0,0,0 };
+
+        int[] lottoNumber_count = new int[46];
         List<LottoResult> lottoResults = new List<LottoResult>();
+
 
         public Frm_ByNumberChart()
         {
@@ -33,7 +41,60 @@ namespace Lotto
 
         private void Frm_ByNumberChart_Load(object sender, EventArgs e)
         {
+            for (int i = lottoResults.Count; i > 0; i--)
+            {
+                cbo_Start_Number.Items.Add(i);
+                cbo_End_Number.Items.Add(i);
+            }
 
+            cbo_Start_Number.Text = 1.ToString();
+            cbo_End_Number.Text = lottoResults.Count.ToString();
+
+            LottoNumber_Count();
+
+        }
+
+        private void LottoNumber_Count()
+        {
+            for (int i = 0; i < lottoNumber_count.Length; i++)
+            {
+                lottoNumber_count[i] = 0;
+            }
+
+            if (Int32.Parse(cbo_Start_Number.Text) > Int32.Parse(cbo_End_Number.Text))
+            {
+                MessageBox.Show("앞자리 숫자가 뒷자리 숫자보다 클 수 없습니다.","error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                for (int i = Int32.Parse(cbo_Start_Number.Text) - 1; i < Int32.Parse(cbo_End_Number.Text); i++)
+                {
+                    lottoNumber_count[lottoResults[i].Number1 - 1]++;
+                    lottoNumber_count[lottoResults[i].Number2 - 1]++;
+                    lottoNumber_count[lottoResults[i].Number3 - 1]++;
+                    lottoNumber_count[lottoResults[i].Number4 - 1]++;
+                    lottoNumber_count[lottoResults[i].Number5 - 1]++;
+                    lottoNumber_count[lottoResults[i].Number6 - 1]++;
+                    if (!chk_Bonus.Checked)
+                    {
+                        lottoNumber_count[lottoResults[i].Bonus - 1]++;
+                    }
+
+                }
+            }
+            ByNumberChart.Series[0].Name = "당첨 횟수";
+            ByNumberChart.Series[0].Points.DataBindY(lottoNumber_count);
+
+            ByNumberChart.ChartAreas[0].AxisX.Interval = 1;
+
+            ByNumberChart.ChartAreas[0].AxisX.Maximum = 45;
+            ByNumberChart.ChartAreas[0].AxisX.Minimum = 1;
+          
+        }
+
+        private void btn_sub_Click(object sender, EventArgs e)
+        {
+            LottoNumber_Count();
         }
     }
 }
